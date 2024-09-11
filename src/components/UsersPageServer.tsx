5287692aead3b0ffa0d8ccad26e3d0511d2a4de5
jason ronda <jay.ronda@symph.co>
2024-09-11 22:23:11 +0800

    clean unused props

--- src/components/UsersPageServer.tsx
+++ src/components/UsersPageServer.tsx
@@ -1,11 +1,5 @@
 import { fetchUsers } from '@/services/user.service';
 import UsersPage from './UsersPage';
-import { User } from '@/types/user.types';
-
-interface UsersPageServerProps {
-  initialUsers: User[];
-  initialTotalPages: number;
-}
 
 const UsersPageServer = async () => {
   const initialData = await fetchUsers(1);
